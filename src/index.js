fea8027f6c4f42edab010e529237eeef9f925ac9
Aurelien Xavier Payong D <xavier.payong@gmail.com>
2018-02-08 22:11:41 +0000

    automatically backed up by learn

--- src/index.js
+++ src/index.js
@@ -2,11 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 // IMPORTANT!!!! uncomment the lines below to make your tests work
-/*
+
+var meInReact
+
 ReactDOM.render(
   meInReact,
   document.getElementById('global')
 );
 
 export default meInReact
-*/
+
